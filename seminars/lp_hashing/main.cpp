5f2471a56e59c145b550023f1403f80fd3506072
Diana Markova <34967260+HeavyHelium@users.noreply.github.com>
2022-01-14 18:42:44 +0200

    seminar files

---
+++ seminars/lp_hashing/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "open_addressing_hash.hpp"
+
+
+int main() {
+
+    return 0;
+}
---
+++ seminars/lp_hashing/open_addressing_hash.hpp
@@ -0,0 +1,9 @@
+#ifndef LP_HASHING_OPEN_ADDRESSING_HASH_HPP
+#define LP_HASHING_OPEN_ADDRESSING_HASH_HPP
+#include <functional>
+#include <utility>
+#include <algorithm>
+
+//load factor = el_cnt + deleted_cnt / bucket_cnt < 0.7
+//resize_factor = elements_cnt / buckets_cnt
+#endif //LP_HASHING_OPEN_ADDRESSING_HASH_HPP
