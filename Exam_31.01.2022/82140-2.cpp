b0ae46a8b0b5c8befb7216acd061f094e8b3255c
Diana Markova <34967260+HeavyHelium@users.noreply.github.com>
2022-01-30 15:40:34 +0200

    added files

---
+++ Exam_31.01.2022/82140-2.cpp
@@ -0,0 +1,41 @@
+//
+// СУ "Св. Климент Охридски"
+// Факултет по математика и информатика
+//
+// Курсове Структури от данни и програмиране
+//         Структури от данни
+// Зимен семестър 2021/22 г.
+//
+// Практически изпит
+//
+// Име: Диана Венциславова Маркова
+// ФН: 82140
+// Специалност: Компютърни науки
+// Курс: 2(втори)
+// Административна група: 1(първа)
+// Ден, в който се явявате на изпита: 31.01.2022(понеделник)
+// Начален час на изпита: 09:00
+// Кой компилатор използвате: g++
+//
+//#define _CRT_SECURE_NO_WARNINGS
+
+#include <cassert>
+#include <iostream>
+#include <stdexcept>
+//#include <fstream>
+//#include <cstddef>
+//#include <algorithm>
+//#include <utility>
+//#include <vector>
+//#include <string>
+
+int main() try {
+
+    std::cout << "hello world! task 2\n";
+
+    return 0;
+} catch(const std::exception& e) {
+    std::cerr << "the following error occurred: ";
+    std::cerr << e.what() << '\n';
+    return -1;
+}
